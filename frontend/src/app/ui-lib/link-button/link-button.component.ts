ee1a03a02e1f4502f96b5824041e7530bdbac9e7
Jeffry Gonzalez <jeff@hypertheory.com>
2022-07-05 16:29:50 -0400

    done with day 1

--- frontend/src/app/ui-lib/link-button/link-button.component.ts
+++ frontend/src/app/ui-lib/link-button/link-button.component.ts
@@ -1,17 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-ui-link-button',
   templateUrl: './link-button.component.html',
-  styleUrls: ['./link-button.component.css']
+  styleUrls: ['./link-button.component.css'],
 })
 export class LinkButtonComponent implements OnInit {
+  @Input() btnStyle: 'primary' | 'danger' | 'success' = 'primary';
+  @Input() btnSize: 'sm' | 'md' | 'lg' = 'md';
+  @Output() btnClicked = new EventEmitter<string>();
+  constructor() {}
 
-@Input() btnStyle: 'primary' | 'danger' | 'success' = 'primary';
-@Input() btnSize: 'sm' | 'md' | 'lg' = 'md';
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  tellPapa() {
+    this.btnClicked.emit('This is from the link button');
   }
-
 }
